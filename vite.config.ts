d71f1a43cacbb6d68e7c0e7011b13fde86d9d5c3
Minh Nhi <minhnhi15102003@gmail.com>
2025-11-21 15:09:23 +0700

    Setup GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),svgr({
+  base: "/admin-fashion-shop/",
+  
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
       svgrOptions: {
-        icon: true, // để svg tự động scale theo viewBox
+        icon: true, // svg tự động scale
       },
-    })],
+    }),
+  ],
 })
